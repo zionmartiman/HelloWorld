0bcecd218eba046f9dbb844937e146ca3171c3e1
unknown <mrocaoliv@gmail.com>
2025-07-10 22:06:06 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'pantalla_detalle.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Navegación con retorno', home: PantallaHome());
+  }
+}
+
+class PantallaHome extends StatefulWidget {
+  @override
+  State<PantallaHome> createState() => _PantallaHomeState();
+}
+
+class _PantallaHomeState extends State<PantallaHome> {
+  int _valorRecibido = 0;
+
+  void _navegarADetalle() async {
+    final resultado = await Navigator.push<int>(
+      context,
+      MaterialPageRoute(builder: (context) => PantallaDetalle(valorInicial: 5)),
+    );
+
+    if (resultado != null) {
+      setState(() {
+        _valorRecibido = resultado;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pantalla Principal')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Valor recibido: $_valorRecibido',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _navegarADetalle,
+              child: Text('Ir a detalle y traer valor'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pantalla_detalle.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class PantallaDetalle extends StatefulWidget {
+  final int valorInicial;
+
+  PantallaDetalle({required this.valorInicial});
+
+  @override
+  _PantallaDetalleState createState() => _PantallaDetalleState();
+}
+
+class _PantallaDetalleState extends State<PantallaDetalle> {
+  late int _contador;
+
+  @override
+  void initState() {
+    super.initState();
+    _contador = widget.valorInicial;
+  }
+
+  void _incrementar() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  void _volverConResultado() {
+    Navigator.pop(context, _contador);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pantalla Detalle')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador: $_contador', style: TextStyle(fontSize: 30)),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _incrementar, child: Text('Sumar 1')),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _volverConResultado,
+              child: Text('Volver y enviar valor'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
