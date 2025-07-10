4e1757f571193daef67040e3e865ec37e8fc2213
unknown <mrocaoliv@gmail.com>
2025-07-10 23:50:54 +0200

    add card

--- lib/posicioning.dart
+++ lib/posicioning.dart
@@ -8,23 +8,81 @@ class Posicioning extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Layouts')),
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
+        child: Column(
           children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=3'),
-            ),
-            Positioned(
-              right: 0,
-              top: 0,
-              child: Container(
-                padding: EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                    'https://i.pravatar.cc/150?img=3',
+                  ),
                 ),
-                child: Text('1', style: TextStyle(color: Colors.white)),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text('1', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 109, 174, 249),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(
+                        255,
+                        0,
+                        0,
+                        0,
+                      ).withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 35,
+                      backgroundImage: NetworkImage(
+                        'https://i.pravatar.cc/150?img=5',
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Juan Pérez',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text('Desarrollador Flutter'),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(onPressed: () {}, child: Text('Perfil')),
+                  ],
+                ),
               ),
             ),
           ],
